4adb7b12279ad378a6ec235da838393fa6463de9
PawelPel-ops <pawell1990@onet.eu>
2021-11-08 13:10:55 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,9 @@
 import React, { useState, Suspense, lazy } from 'react'
 import Nav from '../components/Nav'
 import Hero from '../components/Hero'
-// import About from '../components/About'
-// import FirstQuote from '../components/FirstQuote'
-// import Icons from '../components/Icons'
-// import SecondQuote from '../components/SecondQuote'
-// import ThreeCards from '../components/ThreeCards'
-// import Prices from '../components/Prices'
-// import Contact from '../components/Contact'
 import Gallery from '../components/Gallery'
 import Modal from '../components/Gallery/Modal'
 import { Images } from '../components/Gallery/GalleryData'
-// import Comments from '../components/Comments'
-// import Footer from '../components/Footer'
 
 const About = lazy(() => import('../components/About'))
 const FirstQuote = lazy(() => import('../components/FirstQuote'))
@@ -21,8 +12,6 @@ const SecondQuote = lazy(() => import('../components/SecondQuote'))
 const ThreeCards = lazy(() => import('../components/ThreeCards'))
 const Prices = lazy(() => import('../components/Prices'))
 const Contact = lazy(() => import('../components/Contact'))
-// const Gallery = lazy(() => import('../components/Gallery'))
-// const Modal = lazy(() => import('../components/Gallery/Modal'))
 const Comments = lazy(() => import('../components/Comments'))
 const Footer = lazy(() => import('../components/Footer'))
 
